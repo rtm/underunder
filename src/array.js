1f7573943db1cd010faec3b32c8f020e871996ed
hrishikeshs <hrish2006@gmail.com>
2014-08-20 20:59:58 +0530

    Add some more array tests.

--- src/array.js
+++ src/array.js
@@ -1,124 +1,129 @@
 /* jshint bitwise: false */
+/* global __ */
+var __ = require('./underunder');
 
 __.array={
-    is            : function(a){
-        return Array.isArray(a);
-    },
-    from          : function(){
-	//ES6 Array.from equivalent
-	return this.to.call(this,arguments);
-    },
-    to            : function(a,n){
-	//ES6 Array.from equivalent
-	return Array.prototype.slice.call(a,n||0);
-    },
-    head          : function(a){
-        return a&&a[0];
-    },
-    tail          : function(a){
-        //return all but first element; useful on arguments
-        return a&&[].slice.call(a,1);
-    },
-    drop          : function(a){
-	//pop, returning array
-	a.length--;
-	return a;
-    },
-    unshift       : function(a){
-        //version of unshift which returns array, not new length
-        [].unshift.apply(a,this.to(arguments,1));
-        return a;
-    },
-    push          : function(a){
-        //version of push which returns array, not new length
-        [].push.apply(a,this.to(arguments,1));
-        return a;
-    },
-    omit          : function(a,v){
-	//omit particular values
-	return a.filter(function(vv){return v!==vv;});
-    },
-    overlay       : function(a1,a2,start){
-	//overlay the elements on a2 onto a1 (returning new array)
-	start=start||0;
-	return a1.slice(0,start).concat(a2).concat(a1.slice(start+a2.length));
-    },
-    defaults      : function(a1,a2){
-	//fill in "missing" elts in a1 with a2
-	return a1.concat(a2.slice(a1.length));
-    },
-    unique        : function(a){
-        return a.filter(function(e,p){return a.indexOf(e)===p;});
-    },
-    pluck         : function(a,p){
-	//from array of objects, create array of values for key named "p"
-	//shouldn't this be in __.object?
-        return a.map(function(v){return v[p];});
-    },
-    range         : function(n,n1){
-        //create an integer range from 0..n, or n..n1
-        var arr,ret;
-        if(!n1){n1=n;n=0;}
-        arr=Array.apply(0,Array(Math.abs(n1-n)));
-        ret=arr.map(function(v,i){return n<=n1 ? i+n : n-i;});
-        return ret;
-    },
-    forEach2      : function(a,fn,ctxt){
-        //iterate over an array twice in nested fashion
-        a.forEach(function(i,ii,a){
-            a.forEach(function(j,jj,a){
-                fn.call(ctxt,i,j,ii,jj,a);
-            });
-        });
-    },
-    find_index    : function(a,fn,ctxt){
-        //return the index of the first element in an array that satisfies some conditions
-	//in ECMAScript 6, Array.prototype.findIndex
-        var result=-1;
-        a.some(function(v,i,a){
-            if(fn.call(ctxt,v,i,a)){
-                result=i;
-                return true;
-            }
-        });
-        return result;
-    },
-    find_value    : function(a,fn,ctxt){
-	//return a value if found in the array, otherwise undefined
-	//in ECMAScript 6, Array.prototype.find
-        return a[this.find_index(a,fn,ctxt)];
-    },
-    count         : function(a,fn){
-	//count the number of times a function returns true
-	//function is called with (value,index,array)
-	return a.reduce(function(prev,cur,i,a){return prev+!!fn(cur,i,a);},0);
-    },
-    repeat        : function(i,n){
-        //"static" function to create an array of n instances of i
-        return this.range(n).map(__.fn.fixed(i));
-    },
-    map           : function(a,fn,ctxt){
-	//non-member version of map for composition etc.
-	return a.map(fn,ctxt);
-    },
-    map_to_object : function(a,fn,ctxt){
-        //create an object whose properties are the values in the array, their value the result of a fn call
-	return a.reduce(
-	    function(prev,cur,i,a){
-		return prev[cur]=fn.call(ctxt,cur,i,a), prev;
-	    },
-	    {}
-	);
-        //			return __.object.from_keys_values(a,a.map(fn,ctxt)); //hopefull, above is faster?
-    },
-    contains      : function(a,v){
-        return ~a.indexOf(v);
-    },
-    does_not_contain : function(a,v){
-        return !~a.indexOf(v);
-    },
-    coerce        : function(x){
-        //make scalar into array, leave array alone
-        return [].concat(x);
-    }
+
+  is            : function(a){
+    return Array.isArray(a);
+  },
+  from          : function(){
+    //ES6 Array.from equivalent
+    return this.to.call(this,arguments);
+  },
+  to            : function(a,n){
+    //ES6 Array.from equivalent
+    return Array.prototype.slice.call(a,n||0);
+  },
+  head          : function(a){
+    return a&&a[0];
+  },
+  tail          : function(a){
+    //return all but first element; useful on arguments
+    return a&&[].slice.call(a,1);
+  },
+  drop          : function(a){
+    //pop, returning array
+    a.length--;
+    return a;
+  },
+  unshift       : function(a){
+    //version of unshift which returns array, not new length
+    [].unshift.apply(a,this.to(arguments,1));
+    return a;
+  },
+  push          : function(a){
+    //version of push which returns array, not new length
+    [].push.apply(a,this.to(arguments,1));
+    return a;
+  },
+  omit          : function(a,v){
+    //omit particular values
+    return a.filter(function(vv){return v!==vv;});
+  },
+  overlay       : function(a1,a2,start){
+    //overlay the elements on a2 onto a1 (returning new array)
+    start=start||0;
+    return a1.slice(0,start).concat(a2).concat(a1.slice(start+a2.length));
+  },
+  defaults      : function(a1,a2){
+    //fill in "missing" elts in a1 with a2
+    return a1.concat(a2.slice(a1.length));
+  },
+  unique        : function(a){
+    return a.filter(function(e,p){return a.indexOf(e)===p;});
+  },
+  pluck         : function(a,p){
+    //from array of objects, create array of values for key named "p"
+    //shouldn't this be in __.object?
+    return a.map(function(v){return v[p];});
+  },
+  range         : function(n,n1){
+    //create an integer range from 0..n, or n..n1
+    var arr,ret;
+    if(!n1){n1=n;n=0;}
+    arr=Array.apply(0,Array(Math.abs(n1-n)));
+    ret=arr.map(function(v,i){return n<=n1 ? i+n : n-i;});
+    return ret;
+  },
+  forEach2      : function(a,fn,ctxt){
+    //iterate over an array twice in nested fashion
+    a.forEach(function(i,ii,a){
+      a.forEach(function(j,jj,a){
+        fn.call(ctxt,i,j,ii,jj,a);
+      });
+    });
+  },
+  find_index    : function(a,fn,ctxt){
+    //return the index of the first element in an array that satisfies some conditions
+    //in ECMAScript 6, Array.prototype.findIndex
+    var result=-1;
+    a.some(function(v,i,a){
+      if(fn.call(ctxt,v,i,a)){
+        result=i;
+        return true;
+      }
+    });
+    return result;
+  },
+  find_value    : function(a,fn,ctxt){
+    //return a value if found in the array, otherwise undefined
+    //in ECMAScript 6, Array.prototype.find
+    return a[this.find_index(a,fn,ctxt)];
+  },
+  count         : function(a,fn){
+    //count the number of times a function returns true
+    //function is called with (value,index,array)
+    return a.reduce(function(prev,cur,i,a){return prev+!!fn(cur,i,a);},0);
+  },
+  repeat        : function(i,n){
+    //"static" function to create an array of n instances of i
+    return this.range(n).map(__.fn.fixed(i));
+  },
+  map           : function(a,fn,ctxt){
+    //non-member version of map for composition etc.
+    return a.map(fn,ctxt);
+  },
+  map_to_object : function(a,fn,ctxt){
+    //create an object whose properties are the values in the array, their value the result of a fn call
+    return a.reduce(
+      function(prev,cur,i,a){
+	return prev[cur]=fn.call(ctxt,cur,i,a), prev;
+      },
+      {}
+    );
+    //			return __.object.from_keys_values(a,a.map(fn,ctxt)); //hopefull, above is faster?
+  },
+  contains      : function(a,v){
+    return ~a.indexOf(v);
+  },
+  does_not_contain : function(a,v){
+    return !~a.indexOf(v);
+  },
+  coerce        : function(x){
+    //make scalar into array, leave array alone
+    return [].concat(x);
+  }
 };
+
+module.exports = __.array;
--- tests/array_test.js
+++ tests/array_test.js
@@ -3,18 +3,44 @@ var array  = require('../src/array');
 
 describe('#array_test', function() {
 
-    it('returns true if value passed in is an array', function() {
-       array.is([1,2,3]).should.equal(true);
-    });
-    
-    it('converts an array-like object into an array', function() {
-        arguments = [1,2,3];
-       array.is(array.from(arguments)).should.equal(true);
-    });
+  it('returns true if value passed in is an array', function() {
+    array.is([1,2,3]).should.equal(true);
+  });
+  
+  it('converts an array-like object into an array', function() {
+    arguments = {first: 1, second: 2, third: 3};
+    arguments.length = 3;
+    array.is(array.from(arguments)).should.equal(true);
+  });
 
-    it('returns the first element of the array passed in', function() {
-        console.log("Here.");
-        array.head([1,2,3]).should.equal(1);
-    });
-    
+  it('returns the first element of the array passed in (car)', function() {
+    array.head([1,2,3]).should.equal(1);
+  });
+
+  it('returns all elements except the first (cdr)', function() {
+    var cdr = array.tail([1,2,3,4]);
+    cdr[0].should.equal(2);
+    cdr[1].should.equal(3);
+    cdr[2].should.equal(4);
+  });
+  
+  it('returns the array with last element popped', function() {
+    var arr = [1,2,3,4];
+    var arr_len = arr.length;
+    var popped_array = array.drop(arr);
+    popped_array.length.should.equal(arr_len -1);
+    array.is(popped_array).should.equal(true);
+    popped_array.should.equal(arr); //test they are both the same reference
+    popped_array[0].should.equal(1);
+    popped_array[1].should.equal(2);
+    popped_array[2].should.equal(3);
+  });
+
+  it('returns the unshifted array after unshift is called', function() {
+    var arr = [1,2,3];
+    var unshifted_array = array.unshift(arr, 4);
+    unshifted_array.should.equal(arr); //test they are both the same reference
+    unshifted_array[0].should.equal(4);
+    unshifted_array[unshifted_array.length -1].should.equal(3);
+  });
 });
