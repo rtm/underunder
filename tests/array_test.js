3c0bd7fd27e6fd45e69aa0f47c956a3089e40fba
hrishikeshs <hrish2006@gmail.com>
2014-08-31 12:09:47 +0530

    More tests

--- tests/array_test.js
+++ tests/array_test.js
@@ -43,4 +43,77 @@ describe('#array_test', function() {
     unshifted_array[0].should.equal(4);
     unshifted_array[unshifted_array.length -1].should.equal(3);
   });
+  
+  it('returns the array formed after an element is pushed', function() {
+    var arr = [1, 2, 3, 4];
+    var new_array = array.push(arr, 5);
+    new_array.length.should.equal(5);
+    new_array[new_array.length -1].should.equal(5);
+    });
+  
+  it('returns the new array with specified value omitted', function() {
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var result = array.omit(arr, 2);
+    array.contains(result, 2).should.equal(0);
+  });
+
+  it('overlays the elements of second array on to the first array starting from specified index', function() {
+    var arr1 = [1,2,3,4,5];
+    var arr2 = [10, 20, 30, 40, 50];
+    var result = array.overlay(arr1, arr2, 3);
+    var expectedResults = [1, 2, 3, 10, 20, 30, 40, 50];
+    for(var i = 0, j = expectedResults.length; i < j; i++) {
+      result[i].should.equal(expectedResults[i]);
+    }
+  });
+  
+  xit('fills in missing elements into array1, with elements in array2', function() {
+    var arr1 = [1, 2, 3, 4, 5];
+    var arr2 = [10, 20, 30, 40, 50];
+    var result = array.overlay(arr1, arr2, 3);
+    var expectedResults = [1, 2, 3, 10, 20, 30, 40, 50];
+    var res = array.defaults(arr1, arr2);
+    console.log(res);
+    return true;
+  });
+
+  it('transforms an array with duplicates into a set', function() {
+    var arr1 = [1, 2, 3, 4, 5, 6, 7, 3, 5, 6];
+    var res = array.unique(arr1);
+    res.length.should.equal(7);
+  });
+  
+  it('creates a range from a to b', function() {
+    var range = array.range(10,100);
+    for(var i = 10, j = 0; i < 100, j < range.length; i++, j++) {
+      range[j].should.equal(i);
+    }
+  });
+  
+  it('returns the index of the first element in the array that satisfies some conditions', function() {
+    var arr = [1, 2, 3, 4, 5, 15];
+    array.find_index(arr, function cond(x) { return x%15 === 0; }).should.equal(5); 
+    });
+  
+  it('returns the value in the array that satisfies the condition specified', function() {
+    var arr = [1, 2, 3, 4, 5, 6];
+    array.find_value(arr, function(x) { return x%6 ===0; }).should.equal(6);
+    });
+
+  it('counts the elements in the collection that satisfy the condition', function() {
+    var arr = array.range(1, 100);
+    array.count(arr, function is_even(x) { return x%2 === 0; }).should.equal(49);
+  });
+  
+  xit('creats n instances of the passed in array', function() {
+    var arr = array.range(10);
+    console.log(array.repeat(arr, 5));
+    return true;
+  });
+
+ it('coerces the passed in scalar into an array', function() {
+   var s = 5;
+   var arr = array.coerce(s);
+   array.is(arr).should.equal(true);
+ });
 });
